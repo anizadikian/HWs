28db59b84b54752a10c67056906d26c5013ceb4a
anizadikian <48724834+anizadikian@users.noreply.github.com>
2019-09-10 19:54:21 +0400

    Create hw3.py

---
+++ hw3.py
@@ -0,0 +1,48 @@
+class Classroom:
+
+    def __init__(self, numberofseats, classromnumber):
+        self.nofseats = numberofseats
+        self.nclassrom = classromnumber
+        self.schedule = []
+
+    def isFree(self, start, end):
+        is_Free = True
+        for i in self.schedule:
+            if end < i["start"] or start > i["end"]:
+                pass
+            else:
+                is_Free = False
+                break
+        return is_Free
+
+    def book(self, start, end):
+        self.classstart = start
+        self.classend = end
+        print("Start of the class: ", self.classstart, "Finish class: ", self.classend)
+
+    def reserve(self, start, end):
+        if (self.isFree(start, end)):
+            timeslot = {"start": start, "end": end}
+            self.schedule.append(timeslot)
+            return True
+        else:
+            return False
+
+
+def main():
+
+    myclass = Classroom(30, 212)
+
+    while True:
+        start = input("Please insert the start time of your reservation  ")
+        end = input("Please insert the end time of your reservation   ")
+
+        if myclass.isFree(start, end) == False:
+            print("Sorry, this room is already booked during this period, select another time")
+        else:
+            myclass.reserve(start, end)
+
+        print(myclass.schedule)
+
+
+main()
